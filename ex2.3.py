70df8ddafb2c352bea571560bbc9a837037254e7
liuzhukun <liuzhukun@smartisan.com>
2015-07-03 22:03:35 +0800

    exercise  chapter2

---
+++ ex2.3.py
@@ -0,0 +1,6 @@
+inputhour = raw_input('Enter Hours : ')
+hours=float(inputhour)
+inputrate = raw_input('Enter Rate')
+rate=float(inputrate)
+pay = hours * rate
+print pay
---
+++ ex2.5.py
@@ -0,0 +1,4 @@
+incel = raw_input('enter the celsius\n')
+cel = float(incel)
+fah = cel*9/5 + 32
+print "So, your cel is  ",fah
